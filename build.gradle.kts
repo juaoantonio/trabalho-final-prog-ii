e59429c74a74d1b57719f3f481831abfcba21463
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-09-07 15:50:37 -0300

    feat: initialize Spring Boot application with basic configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,40 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "br.com.joaobarbosa"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.flywaydb:flyway-core")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
---
+++ src/main/java/br/com/joaobarbosa/TicketManagerApplication.java
@@ -0,0 +1,12 @@
+package br.com.joaobarbosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TicketManagerApplication.class, args);
+  }
+}
