ff72d755656a16b49483ca7a9044d9f92728adc8
Luiz Nery <luiz.nery@icen.ufpa.br>
2025-09-09 09:55:44 -0300

    git commit -m "feat(room-crud): criar CRUD completo para Room com UUID
    - Adicionada entidade Room.java com ID do tipo UUID
    - Criado RoomRepository.java estendendo JpaRepository<Room, UUID>
    - Implementado RoomService.java com métodos de CRUD (create, read, update, delete)
    - Implementado RoomController.java com endpoints REST para gerenciamento de salas"

---
+++ src/main/java/br/com/joaobarbosa/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package br.com.joaobarbosa.repository;
+
+import br.com.joaobarbosa.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, UUID> {
+}
