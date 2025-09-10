97e8fe05f985ad122e1c7e42695b6f997c19dfc4
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-09-10 20:55:33 -0300

    refactor: improve code formatting and style with spotless

--- src/main/java/br/com/joaobarbosa/modules/users/User.java
+++ src/main/java/br/com/joaobarbosa/modules/users/User.java
@@ -1,10 +1,8 @@
 package br.com.joaobarbosa.modules.users;
 
 import jakarta.persistence.*;
-
 import java.time.Instant;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import lombok.*;
@@ -21,45 +19,46 @@ import org.springframework.security.core.userdetails.UserDetails;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-  @Column(unique = true, updatable = false, nullable = false)
-  private String username;
+    @Column(unique = true, updatable = false, nullable = false)
+    private String username;
 
-  @Column(nullable = false, name = "hashed_password")
-  private String hashedPassword;
+    @Column(nullable = false, name = "hashed_password")
+    private String hashedPassword;
 
-  @Column(nullable = false)
-  @Enumerated(EnumType.STRING)
-  private UserRole role = UserRole.USER;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
 
-  @CreationTimestamp
-  @Column(name = "created_at", updatable = false)
-  private Instant createdAt;
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
 
-  @UpdateTimestamp
-  @Column(name = "updated_at")
-  private Instant updatedAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 
-  public User(String username, String encryptedPassword, UserRole role) {
-    this.username = username;
-    this.hashedPassword = encryptedPassword;
-    this.role = role;
-  }
+    public User(String username, String encryptedPassword, UserRole role) {
+        this.username = username;
+        this.hashedPassword = encryptedPassword;
+        this.role = role;
+    }
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    if (this.role == UserRole.ADMIN)
-      return List.of(
-          new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN)
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER"));
 
-    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-  }
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
 
-  @Override
-  public String getPassword() {
-    return this.hashedPassword;
-  }
+    @Override
+    public String getPassword() {
+        return this.hashedPassword;
+    }
 }
--- src/main/java/br/com/joaobarbosa/shared/value_objects/Money.java
+++ src/main/java/br/com/joaobarbosa/shared/value_objects/Money.java
@@ -18,84 +18,86 @@ import lombok.NoArgsConstructor;
 @Embeddable
 @NoArgsConstructor
 public class Money {
-  private static final Currency DEFAULT_CURRENCY = Currency.getInstance("BRL");
-  public static final Money ZERO = new Money(BigDecimal.ZERO, DEFAULT_CURRENCY);
+    private static final Currency DEFAULT_CURRENCY = Currency.getInstance("BRL");
+    public static final Money ZERO = new Money(BigDecimal.ZERO, DEFAULT_CURRENCY);
 
-	@Column(name = "money_value", precision = 19, scale = 2, nullable = false)
-  private BigDecimal value;
+    @Column(name = "money_value", precision = 19, scale = 2, nullable = false)
+    private BigDecimal value;
 
-	@Column(name = "money_currency_code", nullable = false, length = 3)
-  private Currency currencyCode;
+    @Column(name = "money_currency_code", nullable = false, length = 3)
+    private Currency currencyCode;
 
-  public Money(Integer value, String currencyCode) {
-    this.value = BigDecimal.valueOf(value);
-    this.currencyCode = Currency.getInstance(currencyCode);
-  }
-
-  public Money(Double value, String currencyCode) {
-    this.value = BigDecimal.valueOf(value);
-    this.currencyCode = Currency.getInstance(currencyCode);
-  }
-
-  public Money(Integer value) {
-    this.value = BigDecimal.valueOf(value);
-    this.currencyCode = DEFAULT_CURRENCY;
-  }
-
-  public Money(Double value) {
-    this.value = BigDecimal.valueOf(value);
-    this.currencyCode = DEFAULT_CURRENCY;
-  }
-
-  public Money add(Money other) {
-    if (!this.currencyCode.equals(other.currencyCode)) {
-      throw new IllegalArgumentException("Não é possível somar valores com moedas diferentes.");
+    public Money(Integer value, String currencyCode) {
+        this.value = BigDecimal.valueOf(value);
+        this.currencyCode = Currency.getInstance(currencyCode);
     }
-    return new Money(this.value.add(other.value), this.currencyCode);
-  }
 
-  public Money subtract(Money other) {
-    if (!this.currencyCode.equals(other.currencyCode)) {
-      throw new IllegalArgumentException("Não é possível subtrair valores com moedas diferentes.");
+    public Money(Double value, String currencyCode) {
+        this.value = BigDecimal.valueOf(value);
+        this.currencyCode = Currency.getInstance(currencyCode);
     }
-    return new Money(this.value.subtract(other.value), this.currencyCode);
-  }
 
-  public Money multiply(BigDecimal factor) {
-    return new Money(this.value.multiply(factor), this.currencyCode);
-  }
-
-  public Money multiply(Money factor) {
-    if (!this.currencyCode.equals(factor.currencyCode)) {
-      throw new IllegalArgumentException(
-          "Não é possível multiplicar valores com moedas diferentes.");
+    public Money(Integer value) {
+        this.value = BigDecimal.valueOf(value);
+        this.currencyCode = DEFAULT_CURRENCY;
     }
-    return new Money(this.value.multiply(factor.value), this.currencyCode);
-  }
 
-  public Money minZero() {
-    if (this.value.compareTo(BigDecimal.ZERO) < 0) {
-      return new Money(BigDecimal.ZERO, this.currencyCode);
+    public Money(Double value) {
+        this.value = BigDecimal.valueOf(value);
+        this.currencyCode = DEFAULT_CURRENCY;
     }
-    return this;
-  }
 
-  @Override
-  public String toString() {
-    Locale locale = guessLocale(this.currencyCode);
-    DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
-    symbols.setCurrencySymbol(this.currencyCode.getSymbol(locale));
+    public Money add(Money other) {
+        if (!this.currencyCode.equals(other.currencyCode)) {
+            throw new IllegalArgumentException(
+                    "Não é possível somar valores com moedas diferentes.");
+        }
+        return new Money(this.value.add(other.value), this.currencyCode);
+    }
 
-    DecimalFormat df = new DecimalFormat("¤ #,##0.00", symbols);
-    return df.format(value);
-  }
+    public Money subtract(Money other) {
+        if (!this.currencyCode.equals(other.currencyCode)) {
+            throw new IllegalArgumentException(
+                    "Não é possível subtrair valores com moedas diferentes.");
+        }
+        return new Money(this.value.subtract(other.value), this.currencyCode);
+    }
 
-  private Locale guessLocale(Currency currency) {
-    return switch (currency.getCurrencyCode()) {
-      case "USD" -> Locale.US;
-      case "EUR" -> Locale.GERMANY;
-      case "BRL" -> Locale.of("pt", "BR");
-      default -> Locale.getDefault();
-    };
-  }
+    public Money multiply(BigDecimal factor) {
+        return new Money(this.value.multiply(factor), this.currencyCode);
+    }
+
+    public Money multiply(Money factor) {
+        if (!this.currencyCode.equals(factor.currencyCode)) {
+            throw new IllegalArgumentException(
+                    "Não é possível multiplicar valores com moedas diferentes.");
+        }
+        return new Money(this.value.multiply(factor.value), this.currencyCode);
+    }
+
+    public Money minZero() {
+        if (this.value.compareTo(BigDecimal.ZERO) < 0) {
+            return new Money(BigDecimal.ZERO, this.currencyCode);
+        }
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        Locale locale = guessLocale(this.currencyCode);
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
+        symbols.setCurrencySymbol(this.currencyCode.getSymbol(locale));
+
+        DecimalFormat df = new DecimalFormat("¤ #,##0.00", symbols);
+        return df.format(value);
+    }
+
+    private Locale guessLocale(Currency currency) {
+        return switch (currency.getCurrencyCode()) {
+            case "USD" -> Locale.US;
+            case "EUR" -> Locale.GERMANY;
+            case "BRL" -> Locale.of("pt", "BR");
+            default -> Locale.getDefault();
+        };
+    }
 }
