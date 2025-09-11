4641033dffda7d543b2db4a3b3702441fa62e47e
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-09-11 15:29:43 -0300

    refactor: clean up formatting and organization in Coupon and CouponType classes

--- src/main/java/br/com/joaobarbosa/modules/coupons/Coupon.java
+++ src/main/java/br/com/joaobarbosa/modules/coupons/Coupon.java
@@ -3,12 +3,11 @@ package br.com.joaobarbosa.modules.coupons;
 import br.com.joaobarbosa.modules.orders.Order;
 import br.com.joaobarbosa.shared.value_objects.Money;
 import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.Instant;
-import java.util.UUID;
 
 @Builder
 @Entity(name = "coupons")
@@ -18,31 +17,31 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 public class Coupon {
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-  @Column(nullable = false, unique = true)
-  private String code;
+    @Column(nullable = false, unique = true)
+    private String code;
 
-  @Enumerated(EnumType.STRING)
-  @Column(nullable = false)
-  private CouponType type;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CouponType type;
 
-  @Column(nullable = false)
-  private Money value;
+    @Column(nullable = false)
+    private Money value;
 
-  @Column(nullable = false, name = "is_active")
-  private Boolean isActive;
+    @Column(nullable = false, name = "is_active")
+    private Boolean isActive;
 
-	@OneToOne(mappedBy = "coupon")
-	private Order order;
+    @OneToOne(mappedBy = "coupon")
+    private Order order;
 
-  @CreationTimestamp
-  @Column(name = "created_at", updatable = false)
-  private Instant createdAt;
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
 
-  @UpdateTimestamp
-  @Column(name = "updated_at")
-  private Instant updatedAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 }
--- src/main/java/br/com/joaobarbosa/modules/coupons/CouponType.java
+++ src/main/java/br/com/joaobarbosa/modules/coupons/CouponType.java
@@ -1,17 +1,12 @@
 package br.com.joaobarbosa.modules.coupons;
 
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
-
-
 public enum CouponType {
-	PERCENT("PERCENT"),
-	FIXED("FIXED");
+    PERCENT("PERCENT"),
+    FIXED("FIXED");
 
-	final String value;
+    final String value;
 
-	CouponType(String value) {
-		this.value = value;
-	}
+    CouponType(String value) {
+        this.value = value;
+    }
 }
