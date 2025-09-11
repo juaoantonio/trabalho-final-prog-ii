ab95656859a7011248f1f5407d90aed0c7490b8b
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-09-10 23:52:39 -0300

    feat: add Room entity with JPA annotations and total capacity calculation

---
+++ src/main/java/br/com/joaobarbosa/entity/Room.java
@@ -0,0 +1,54 @@
+package br.com.joaobarbosa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity(name = "rooms")
+@Table(name = "rooms")
+@Getter
+@Setter
+@Builder(setterPrefix = "with")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false, unique = true)
+  private String name;
+
+  @Column(nullable = false)
+  private Integer rows;
+
+  @Column(nullable = false)
+  private Integer cols;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  private Instant createdAt;
+
+  @UpdateTimestamp
+  @Column(nullable = false)
+  private Instant updatedAt;
+
+  public Room(String name, Integer rows, Integer cols) {
+    this.name = name;
+    this.rows = rows;
+    this.cols = cols;
+  }
+
+  // Método para calcular a capacidade total
+  public Integer getTotalCapacity() {
+    return this.rows * this.cols;
+  }
+}
